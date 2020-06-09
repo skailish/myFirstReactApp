5e02df0d50eab59111ad4793ea18a1a25fa279cd
Daniela Capponi <capponi.daniela@gmail.com>
2020-06-08 21:29:09 -0300

    subo ejercicio de clase: hook the useArray

---
+++ src/clase_81_practicasAvanzadas/App.js
@@ -0,0 +1,47 @@
+import React from "react"
+import useArray from "./hooks/useArray"
+
+const data = [
+  {
+    id: 1,
+    text: "Ir al súper",
+    status: "pending",
+  },
+  {
+    id: 2,
+    text: "Comprar comida",
+    status: "pending",
+  },
+  {
+    id: 3,
+    text: "Preparar cena",
+    status: "pending",
+  },
+]
+
+const App = () => {
+  const [items, methods] = useArray(data)
+
+  return (
+    <>
+      <ul>
+        {items.map((item) => (
+          <li>{JSON.stringify(item)}</li>
+        ))}
+      </ul>
+
+      <button
+        onClick={() =>
+          methods.updateById(3, {
+            text:
+              "Salir a correr como todo el mundo en Palermo en este momento",
+          })
+        }
+      >
+        Agregar
+      </button>
+    </>
+  )
+}
+
+export default App
